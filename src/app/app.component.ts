e9289eb840735602e81056a0aa732d022b848658
Amador (Guitxo) <guitxo@guitxo.com>
2022-02-22 00:00:22 +0100

    ocultar tabs en home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Utils } from './shared/utils.class';
 import * as linksList from '../assets/links.json';
 import { ILink } from './shared/shared.inteface';
-
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { filter } from 'rxjs/operators'
+import { Observable, of, Subscription } from 'rxjs';
 /**
  * Component principal de la aplicación
  */
@@ -11,14 +13,19 @@ import { ILink } from './shared/shared.inteface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   /**
    * Lista de rutas a mostrar
    */
   links!: ILink[];
 
-  constructor() {}
+  routerSubs!: Subscription;
+  showTabs!: Observable<boolean>;
+
+  constructor(
+    private readonly router: Router
+  ) {}
 
   /**
    * OnInit: carga la lista de rutas a mostrar
@@ -27,5 +34,15 @@ export class AppComponent implements OnInit {
     this.links = Utils.getJSONDefault(linksList).sort((a: ILink, b: ILink) => {
       return a.order < b.order ? -1 : 1;
     });
+
+    this.routerSubs = this.router.events
+      .pipe(filter((event: Event) => event instanceof NavigationEnd))
+      .subscribe((event: Event) => {
+        this.showTabs = of((event as NavigationEnd).urlAfterRedirects !== '/home');
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubs.unsubscribe();
   }
 }
